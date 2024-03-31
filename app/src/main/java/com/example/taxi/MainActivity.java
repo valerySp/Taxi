4e323e8487391d90263c0b3b619b62c7a9570559
valerySp <vsparnyuk@yandex.by>
2024-03-31 21:18:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/taxi/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.taxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Thread thread=new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                finally {
+                    Intent intent=new Intent(MainActivity.this,Welcome.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
